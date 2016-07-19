3efed52bb2d49b7596636416a6c9946e49e8a659
Charlie Mathews <charlie@charliemathews.com>
2016-07-19 16:53:36 -0500

    Switched to burger menu.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
 //particlesJS.load('particles', 'assets/particles.json');
 
+// handle loading animation
 $(document).ready(function($) {
+	$("#preloader-wrap").animate({opacity: 0}, 400);
+	setTimeout(function() {$("#loading").animate({opacity: 0}, 200);}, 400);
+	setTimeout(function() {$("#loading").remove();}, 600);
+});
 
-	//hide preloader
-	$("#loading").animate({opacity: 0}, 400);
-	setTimeout(function() {$("#loading").remove();}, 400);
-
+// menu animation
+$("#burger").click(function() {
+  if(!$(this).hasClass("cross")) {
+		$(this).addClass("cross");
+		$("#menu").addClass("open");
+	} else {
+		$(this).removeClass("cross");
+		$("#menu").removeClass("open");
+	}
 });
