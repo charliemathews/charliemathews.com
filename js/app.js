98016927f8514ed9c4524cae4e3a0ffafeff2fe7
Charlie Mathews <charlie@charliemathews.com>
2016-07-19 20:46:00 -0500

    New menu style. Theater mode. Hide fade during menu event.

--- js/app.js
+++ js/app.js
@@ -7,13 +7,32 @@ $(document).ready(function($) {
 	setTimeout(function() {$("#loading").remove();}, 600);
 });
 
+function showMenu() {
+	$("#burger").addClass("cross");
+	$("#menu").addClass("open");
+	$("#nav_curtain").animate({opacity:0.1}, 400);
+	$("#fadeout").animate({opacity:0}, 100);
+}
+
+function hideMenu() {
+	$("#burger").removeClass("cross");
+	$("#menu").removeClass("open");
+	$("#nav_curtain").animate({opacity:0}, 400);
+	$("#fadeout").animate({opacity:1}, 600);
+}
+
 // menu animation
 $("#burger").click(function() {
   if(!$(this).hasClass("cross")) {
-		$(this).addClass("cross");
-		$("#menu").addClass("open");
+		showMenu();
 	} else {
-		$(this).removeClass("cross");
-		$("#menu").removeClass("open");
+		hideMenu();
+	}
+});
+
+// hide menu if user clicks on content? need to improve.
+$("#content").click(function() {
+  if($("#burger").hasClass("cross")) {
+		hideMenu();
 	}
 });
